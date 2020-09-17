c0f27e2aacb881ac4890394ad122f488e85f0b7d
Raisha Lovindeer <raisha.lovindeer@gmail.com>
2020-09-17 09:12:39 -0400

    constrains acclimation parameter between 0-1

--- transform.m
+++ transform.m
@@ -2,10 +2,4 @@ function y = transform(v)
 % y = transform(v)
 % transform dv/dt from -inf to inf and then take the tangent of this funct
     
-if v < 0
-        v = v*-inf;
-else if v> 0
-        v = v*inf;
-    end
-
 y = 0.5+0.5*(tanh(v));
